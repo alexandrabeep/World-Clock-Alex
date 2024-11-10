baf4ded3d4c8f7ddfa5c4155328841d11d56ac05
alexandrabeep <alexpotato6@gmail.com>
2024-11-10 14:25:31 +0100

    Set Interval for the select city

--- javascript/index.js
+++ javascript/index.js
@@ -19,15 +19,20 @@ function updateTime() {
     "HH:mm[<small>]:ss[</small>]"
   )}`;
 }
+updateTime();
+setInterval(updateTime, 1000);
+// Select city
 function updateCity(event) {
+  clearInterval();
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `<div class="city">
+  function changeCityElement() {
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `<div class="city">
           <div>
             <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
@@ -37,11 +42,9 @@ function updateCity(event) {
           )}</div>
         </div>
         <a href="/" id="home-page-link"> All cities </a>`;
+  }
+  setInterval(changeCityElement, 1000);
 }
 
-updateTime();
-setInterval(updateTime, 1000);
-
 let citiesSelectElement = document.querySelector("#city");
-
 citiesSelectElement.addEventListener("change", updateCity);
